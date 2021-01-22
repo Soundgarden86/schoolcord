9e4a5f8f274573ea11c40b821fc5eac51e378112
Soundgarden86 <59464338+Soundgarden86@users.noreply.github.com>
2021-01-22 15:32:28 +0100

    update comments

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+
+// These are the variables to manipulate the objects on the webpage
 let avatar = document.querySelector('.avatar');
 let moveBy = 10;
+
 
 window.addEventListener('load', () => {
     avatar.style.position = 'absolute';
@@ -10,17 +13,28 @@ window.addEventListener('load', () => {
 
 window.addEventListener('keyup', (e) => {
     switch (e.key) {
+            //             If you press the left arrow key the object (avatar) will move to left
         case 'ArrowLeft':
             avatar.style.left = parseInt(avatar.style.left) - moveBy + 'px';
+            //             the command stops immediatly
             break;
+            
+            //             If you press the right arrow key the object (avatar) will move to right
         case 'ArrowRight':
             avatar.style.left = parseInt(avatar.style.left) + moveBy + 'px';
+            //             the command stops immediatly
             break;
+            
+            //             If you press the up arrow key the object (avatar) will move up
         case 'ArrowUp':
             avatar.style.top = parseInt(avatar.style.top) - moveBy + 'px';
+            //             the command stops immediatly
             break;
+            
+            //             If you press the down arrow key the object (avatar) will move down
         case 'ArrowDown':
             avatar.style.top = parseInt(avatar.style.top) + moveBy + 'px';
+            //             the command stops immediatly
             break;
     }
 });
